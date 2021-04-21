7182bf6bddcba55d4d727123e36ab7c589112440
Izaro <670058122@qq.com>
2021-04-21 14:42:19 +0800

    Initial commit

---
+++ spring_1_base/src/com/jt/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.jt.dao;
+
+/**
+ * @Author:Izaro
+ * @Description:
+ * @Date:Created in 11:17 2021/4/21
+ * @Modified By:
+ */
+public class UserDao {
+    public void addUser(){
+        System.out.println("新增一个用户:项思醒");
+    }
+}
---
+++ spring_1_base/src/com/jt/service/UserService.java
@@ -0,0 +1,14 @@
+package com.jt.service;
+
+/**
+ * @Author:Izaro
+ * @Description:
+ * @Date:Created in 11:20 2021/4/21
+ * @Modified By:
+ */
+public interface UserService {
+    /**
+     * 定义行为规范
+     */
+    void addUser();
+}
